4eba8f4960181cc964de9055d66a76045b001181
Guillermo Rodriguez <xile@klm134154.cadra.aero>
2018-12-25 23:06:46 +0100

    Adding Criterion interfaces to implement behaviour

--- src/main/java/com/oguiller/domain/CarScratch.java
+++ src/main/java/com/oguiller/domain/CarScratch.java
@@ -20,6 +20,49 @@ class PassengerCountOrder implements Comparator<Car> {
     }
 }
 
+/**
+ * We have colored cars in one case and cars selected by the amount of gas in them in the other case and you can
+ * probably see, that we could implement car criterion as many ways as we saw fit and the same selection mechanism
+ * would just keep working. We also have the situation that we saw where, with the red car criterion, that
+ * particular selection mechanism, only picks out red cars and the behavior in there is completely without any kind
+ * of state at all. That object only has behavior, has no state. However, for the gas level criterion, we actually
+ * have a little bit of state as well as behavior, but in both cases, the essential purpose of the object we're
+ * passing as an argument is because of its behavior. The state in the gas level one, merely tweaks the behavior
+ * just a little bit. The essence is, that we have passed behavior into a function so that that function can use
+ * the behavior in support of its own behavior. Now interestingly, and you may well already know this, this is not a
+ * new idea in object-oriented programming. When the well-known Gang of Four, Gamma, Helm, Johnson and Vlissides, put
+ * together their design patterns book back in about 1994, they actually described this PAM and they called it, the
+ * command PAM. So, this has a good long pedigree. Turns out, that not only is the command PAM, a well-known PAM,
+ * although not perhaps as well used as it should have been in object orientation, it's also key way of doing things
+ * in functional programming.
+ */
+
+interface CarCriterion {
+    boolean test(Car c);
+}
+
+class RedCarCriterion implements CarCriterion{
+
+    @Override
+    public boolean test(Car c) {
+        return c.getColor().equals("Red");
+    }
+}
+
+class GasLevelCarCriterion  implements CarCriterion {
+
+    private int threshold;
+
+    public GasLevelCarCriterion(int threshold){
+        this.threshold = threshold;
+    }
+
+    @Override
+    public boolean test(Car c) {
+        return c.getGasLevel() >= threshold;
+    }
+}
+
 public class CarScratch {
 
     public static void showAll(Iterable<Car> lc) {
@@ -30,41 +73,18 @@ public class CarScratch {
         System.out.println("-------------------------------");
     }
 
-    /**
-     * Okay, so we've created in fact a new list here. And that was deliberate. We created the new list rather than
-     * modifying the existing one. There are a couple of reasons that functional programming prefers that. One of
-     * them that is pretty simple and fairly obvious is, imagine that I had shared this list with other pieces of my
-     * software. If we had changed the list here, then those other pieces of software would be looking at it going,
-     * "Wait, that can't happen, all the blue cars have disappeared!" So, creating a new list is generally the
-     * functional way and is gonna be something that we're going to do all the way through this class.
-     *
-     * @param in
-     * @return
-     */
-    public static List<Car> getColoredCars(Iterable<Car> in, String color) {
+
+    public static List<Car> getCarsByCriterion(Iterable<Car> in, CarCriterion crit) {
         List<Car> output = new ArrayList<>();
 
         for (Car c : in) {
-            if (c.getColor().equals(color)) {
+            if (crit.test(c)) {
                 output.add(c);
             }
         }
 
         return output;
     }
-
-    public static List<Car> getCarsByGasLevel(Iterable<Car> in, int gasLevel) {
-        List<Car> output = new ArrayList<>();
-
-        for (Car c : in) {
-            if (c.getGasLevel() >= gasLevel) {
-                output.add(c);
-            }
-        }
-
-        return output;
-    }
-
 
     public static void main(String[] args) {
         List<Car> cars = Arrays.asList(
@@ -76,9 +96,11 @@ public class CarScratch {
 
         showAll(cars);
 
-        showAll(getColoredCars(cars, "Red"));
+        showAll(getCarsByCriterion(cars, new RedCarCriterion()));
 
-        cars.sort(new PassengerCountOrder());
+        showAll(getCarsByCriterion(cars, new GasLevelCarCriterion(6)));
+
+//        cars.sort(new PassengerCountOrder());
 
         showAll(cars);
     }
